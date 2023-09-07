9cabcbc16f2a210c43885ae5a83b5b75c12a7cbd
budi-afk <budiman1347@gmail.com>
2023-09-07 21:02:14 +0700

    mastering frontend home

---
+++ resources/views/frontend/home.blade.php
@@ -0,0 +1,39 @@
+@extends('frontend.layouts.app')
+
+@section('content')
+    <!-- Header-Area-Start -->
+    @include('frontend.section.hero')
+    <!-- Header-Area-End -->
+
+    <!-- Service-Area-Start -->
+    @include('frontend.section.service')
+    <!-- Service-Area-End -->
+
+    <!-- About-Area-Start -->
+    @include('frontend.section.about')
+    <!-- About-Area-End -->
+
+    <!-- Portfolio-Area-Start -->
+    @include('frontend.section.portfolio')
+    <!-- Portfolio-Area-End -->
+
+    <!-- Skills-Area-Start -->
+    @include('frontend.section.skills')
+    <!-- Skills-Area-End -->
+
+    <!-- Experience-Area-Start -->
+    @include('frontend.section.experience')
+    <!-- Experience-Area-End -->
+
+    <!-- Testimonial-Area-Start -->
+    @include('frontend.section.testimonial')
+    <!-- Testimonial-Area-End -->
+
+    <!-- Blog-Area-Start -->
+    @include('frontend.section.blog')
+    <!-- Blog-Area-End -->
+
+    <!-- Contact-Area-Start -->
+    @include('frontend.section.contact')
+    <!-- Contact-Area-End -->
+@endsection
